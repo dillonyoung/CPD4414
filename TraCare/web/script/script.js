7b993f1766100c6b43022a4edb3e44a0f9a2e9bc
Dillon Young <info@dillonyoung.ca>
2013-04-11 00:35:25 -0400

    Code Cleanup
    - Cleaned up the appliation code
    - Added custom icons to the toolbar icons

--- TraCare/web/script/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#btn_register").on('click', function () {
-       $.mobile.changePage($('register.jsp'), 'pop', false, true);
-    });
-    var activePage = $.mobile.activePage[0].id;
-
-});
